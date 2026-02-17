f83f053716b3b2cc8d12929b8ed0acac2881c0f2
bora4-netizen <chorkbora4@gmail.com>
2026-02-17 08:52:04 +0700

    login and sig up done

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -2,8 +2,8 @@
 import { ref } from "vue"
 
 
-const email = ref("chorkbora4@gmail.com")
-const password = ref("bora1234")
+const email = ref("")
+const password = ref("")
 
 const handleLogin = () => {
   console.log("Logged In:", email.value)
@@ -12,43 +12,52 @@ const handleLogin = () => {
 </script>
 
 <template>
-  <div class=" flex items-top justify-center pt-20 pb-20 mb-20">
-    <div class="bg-white p-8  shadow-md w-96">
-      <h3 class="text-2xl font-bold mb-6 text-center">Sign your account</h3>
+    <div class="flex items-top justify-center pt-20 pb-20 mb-20">
+    <div class="bg-white w-110 p-8 shadow-md border rounded-lg">
+      <h3 class="text-2xl font-bold mb-6 text-center">Login</h3>
 
-      <form @submit.prevent="handleSignup" class="space-y-4">
-        <input class="w-full p-3 border rounded-lg"
-          v-model="name"
-          type="text"
-          placeholder="Full Name"
-          required
-        />
-        
+      <form @submit.prevent="handleLogin" class="space-y-4">
+
+        <div class="flex justify-center items-center gap-4">
+          <input class="w-full p-3 border rounded-lg"
+            v-model="name"
+            type="text"
+            placeholder="Last Name"
+            required
+          />
+          <input class="w-full p-3 border rounded-lg"
+            v-model="firstName"
+            type="text"
+            placeholder="First Name"
+            required
+          />
+
+        </div>
 
         <input class="w-full p-3 border rounded-lg"
           v-model="email"
           type="email"
-          placeholder="Email"
+          placeholder="Your Email or number phone"
           required
         />
-
         <input class="w-full p-3 border rounded-lg"
           v-model="password"
           type="password"
           placeholder="Password"
           required
         />
+      
 
         
         <button class="w-full bg-blue-600 text-white p-3 rounded-lg" type="submit">
             Sign Up
-        </button> 
+        </button>
     </form>
 
 
       <p class="text-sm text-center mt-4">
         Already have an account?
-        <NuxtLink to="/sign-up" class="text-blue-600">sign-up</NuxtLink>
+        <NuxtLink to="/sign-up" class="text-blue-600">Login</NuxtLink>
       </p>
     </div>
   </div>
--- app/pages/sign-up.vue
+++ app/pages/sign-up.vue
@@ -2,8 +2,8 @@
 import { ref } from "vue"
 
 
-const email = ref("chorkbora4@gmail.com")
-const password = ref("bora1234")
+const email = ref("")
+const password = ref("")
 
 const handleLogin = () => {
   console.log("Logged In:", email.value)
@@ -13,35 +13,54 @@ const handleLogin = () => {
 
 <template>
   <div class="flex items-top justify-center pt-20 pb-20 mb-20">
-    <div class="bg-white p-8 rounded-2xl shadow-md w-96">
+    <div class="bg-white w-110 p-8 shadow-md border rounded-lg">
       <h3 class="text-2xl font-bold mb-6 text-center">Create Account</h3>
 
       <form @submit.prevent="handleSignup" class="space-y-4">
+
+        <div class="flex justify-center items-center gap-4">
+          <input class="w-full p-3 border rounded-lg"
+            v-model="name"
+            type="text"
+            placeholder="Last Name"
+            required
+          />
+          <input class="w-full p-3 border rounded-lg"
+            v-model="firstName"
+            type="text"
+            placeholder="First Name"
+            required
+          />
+
+        </div>
+
         <input class="w-full p-3 border rounded-lg"
-          v-model="name"
-          type="text"
-          placeholder="Full Name"
+          v-model="phoneNumber"
+          type="number phone"
+          placeholder="Your Number Phone"
           required
         />
 
         <input class="w-full p-3 border rounded-lg"
           v-model="email"
           type="email"
-          placeholder="Email"
+          placeholder="Your Email"
           required
         />
-
         <input class="w-full p-3 border rounded-lg"
           v-model="password"
           type="password"
           placeholder="Password"
           required
         />
-
+      
+        <p class="text-sm text-center mt-4">
+          <NuxtLink to="/login" class="text-blue-600">For got password?</NuxtLink>
+        </p>
         
         <button class="w-full bg-blue-600 text-white p-3 rounded-lg" type="submit">
             Sign Up
-        </button> 
+        </button>
     </form>
 
 

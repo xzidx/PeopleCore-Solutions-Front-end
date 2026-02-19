1d20b280bdb5e4fe23954df7acbd2419eee1215a
theasithul <theasithul9@gmail.com>
2026-02-19 07:58:47 +0700

    fix changes

---
+++ app/pages/jobs.vue
@@ -0,0 +1,196 @@
+<template>
+  <div class="relative flex items-center">
+    <img src="/../public/images/banner.jpg"alt="Training"class="absolute inset-0 w-full h-full object-cover"/>
+    <div class="absolute inset-0"></div>
+
+    <div class="relative z-10 max-w-4xl mx-auto px-6 text-black">
+      <h1 class="text-4xl md:text-4xl font-bold mb-6 mt-20 "> TRAINING MATERIAL <br/> DEVELOPMENT</h1>
+
+      <p class="max-w-lg text-gray-500 mb-6 ">
+        PeopleCore is a leading provider of customized training materials
+        that help organizations achieve operational excellence and grow
+        talent. Our approach focuses on how people learn to build strong  
+        learning experiences.
+      </p>
+
+      <button class="bg-blue-700 text-black px-6 py-3 hover:bg-yellow-500 mb-20">TRAINING MATERIAL</button>
+    </div>
+
+  </div>
+  <div class=" mt-16 flex">
+      <div class="">
+        <img src="/../public/images/pic.jpg" alt="">
+      </div>
+      <div class="w-[500px]">
+          <h2 class="text-5xl text-blue-600">FEATURED SERVICE:</h2> <br>
+          <h2 class="text-5xl text-yellow-400">NEW eLEARNING DEVELOPMENT</h2> <br>
+          <img src="/../public/images/pic1.jpg" alt=""> <br><br>
+          <h2 class="text-2xl text-yellow-400">OUR MISSION</h2> <br>
+          <h2 class="text-3xl text-blue-800">Real-World Solution</h2> <br>
+          <p>Your people. Your workplace dynamics. Your outcome. At PeopleCore, we realize that the complexities of your employees, workplace procedures, and habits require highly customized solutions that equip workers with the mindset, skillset, and toolset to improve organizational performance.</p> <br>
+          <div class="flex gap-3">
+              <div class="bg-white flex shadow-md ">  
+                <div class="p-4">
+                  <img src="/../public/images/pic2.jpg" alt="">
+                </div>
+                <div class="p-4 text-xl">Material <br>Development</div>
+              </div>
+              <div class="bg-white flex shadow-md">  
+                <div class="p-4">
+                  <img src="/../public/images/pic3.jpg" alt="">
+                </div>
+                <div class="p-4 text-xl">eLearning <br>Development</div>
+              </div>
+          </div> <br>
+          <div class="flex gap-3">
+              <div class="bg-white shadow-md flex">  
+                <div class="p-4">
+                  <img src="/../public/images/pic4.jpg" alt="">
+                </div>
+                <div class="p-4 text-xl">Leadership <br>Development</div>
+              </div>
+              <div class="bg-white flex shadow-md">  
+                <div class="p-4">
+                  <img src="/../public/images/pic5.jpg" alt="">
+                </div>
+                <div class="p-4 text-xl">Procedure <br>Development</div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <h2 class="text-center mt-10 text-4xl text-gray-500">You need people who have been there. We’ve been there.</h2>
+  <p class="text-center mt-2">In collaboration with clients, we build robust training solutions with a keen focus on delivering measurable results.</p>
+
+  <div class=" mt-10 flex item-center justify-center gap-10">
+      <div class="w-[560px] ">
+           <div class="">
+              <img src="/../public/images/job1.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl">Basic Operator Training</h2> 
+              <p class="mt-3">We develop customized, graphics-driven technical training for petrochemical operators and maintenance workers, and we help ensure implementation. We develop customized, graphics-driven technical training for petrochemical operators and maintenance workers, and we help ensure implementation.</p> 
+              <button class="bg-black text-white px-10 py-2 hover:bg-blue-500 mt-5">learn more</button> 
+           </div>
+      </div>
+      <div class="w-[560px] ">
+           <div class="">
+              <img src="/../public/images/job2.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl">Basic Operator Training</h2> 
+              <p class="mt-3">We develop customized operating and maintenance procedures, formatted and updatable with extreme accuracy and efficiency.We develop customized operating and maintenance procedures, formatted and updatable with extreme accuracy and efficiency.</p> 
+              <button class="bg-black text-white px-10 py-2 hover:bg-blue-500 mt-5">learn more</button> 
+           </div>
+      </div>
+  </div>
+
+  <div class=" mt-10 flex item-center justify-center gap-10">
+      <div class="w-[560px] ">
+           <div class="">
+              <img src="/../public/images/job3.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl">Basic Operator Training</h2> 
+              <p class="mt-3">We deliver highly engaging, immediately applicable training for leaders and soft-skills training for all employees.We deliver highly engaging, immediately applicable training for leaders and soft-skills training for all employees.We deliver highly engaging, immediately applicable training for leaders.</p>  
+              <button class="bg-black text-white px-10 py-2 hover:bg-blue-500 mt-5">learn more</button> 
+           </div>
+      </div>
+      <div class="w-[560px] ">
+           <div class="">
+              <img src="/../public/images/job4.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl">Basic Operator Training</h2> 
+              <p class="mt-3">Leadership is science and an art. Learn more, and create something better with our help.Leadership is science and an art. Learn more, and create something better with our help.Leadership is science and an art. Learn more, and create something better with our help.</p> 
+              <button class="bg-black text-white px-10 py-2 hover:bg-blue-500 mt-5">learn more</button> 
+           </div>
+      </div>
+  </div>
+  <div class="mt-10 flex item-center justify-center gap-10">
+      <div class="w-[560px] ">
+           <div class="">
+              <img src="/../public/images/job5.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl">Basic Operator Training</h2> 
+              <p class="mt-3">Employee training and development is essential for the growth of your organization and the workforce that propels it. Employee training and development is essential for the growth of your organization and the workforce that propels it.</p> 
+              <button class="bg-black text-white px-10 py-2 hover:bg-blue-500 mt-5">learn more</button> 
+           </div>
+      </div>
+      <div class="w-[560px] ">
+           <div class="">
+              <img src="/../public/images/job6.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl">Basic Operator Training</h2> 
+              <p class="mt-3">We help leaders lead their workforce to higher performance levels. We help leaders lead their workforce to higher performance levels.We help leaders lead their workforce to higher performance levels. We help leaders lead their workforce to higher performance levels.</p> 
+              <button class="bg-black text-white px-10 py-2 hover:bg-blue-500 mt-5">learn more</button> 
+           </div>
+      </div>
+  </div>
+
+   <div class=" bg-gray-100 mt-10 flex item-center justify-center gap-10 p-10">
+      <div class="w-[360px] bg-blue-500 p-14 py-14">
+           <div class="mt-5 text-white">
+              <h2 class="text-4xl">Why You Need</h2> <br>
+              <h2 class="text-4xl">PeopleCore</h2> <br>
+              <p class="mt-3">Basic training methodologies lack the means to produce substantial, sustained workplace efficiency. Why is that? Well, they don’t always consider how people learn best.</p> <br>
+              <span class="text-xl">At PeopleCore, we do.</span>
+           </div>
+      </div>
+      <div class="w-[360px] ">
+           <div class="">
+              <img src="/../public/images/pic6.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl">  Visual Material</h2> 
+              <p class="mt-3">Human brains are wired as visual information processors. We provide training that is graphics- driven.</p> 
+           </div>
+      </div>
+      <div class="w-[360px] ">
+           <div class="">
+              <img src="/../public/images/pic7.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl">Concise Content</h2> 
+              <p class="mt-3">Conventional technical writing is inefficient. We use numbered nodes with graphics to minimize text, accelerate learning, and maximize retention.</p>  
+           </div>
+      </div>
+  </div>
+
+   <div class=" bg-gray-100 flex item-center justify-center gap-10 p-10">
+      <div class="w-[360px] ">
+           <div class="">
+              <img src="/../public/images/pic8.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl"> Performance Based</h2> 
+              <p class="mt-3">Learners learn why along with what and how with well trained leaders and instructors.</p> 
+           </div>
+      </div>
+      <div class="w-[360px] ">
+           <div class="">
+              <img src="/../public/images/pic9.jpg" alt="">
+           </div>
+           <div class="mt-5">
+              <h2 class="text-xl">Accurate and Thorough</h2> 
+              <p class="mt-3">Developed by mechanical, chemical and process engineers trained in instructional design.</p>  
+           </div>
+      </div>
+      <div class="w-[360px] bg-blue-500 p-14 py-20">
+           <div class="mt-5 text-white p-3">
+              <h2 class="text-4xl">Customized</h2> <br>
+              <h2 class="text-4xl">Training,</h2> <br>
+              <h2 class="text-4xl">Designed for</h2> <br>
+              <h2 class="text-4xl">Impact.</h2> 
+           </div>
+      </div>
+  </div>
+
+  <div class="bg-red-500 flex item-center justify-center">
+      <div class="bg-blue-200">hsfg</div>
+      <div class="bg-white">hahah</div>
+  </div>
+   
+</template>
+  

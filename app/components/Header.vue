d28b18004190bfaba51d46bb79e8d19c157bd97f
xzidx <c.samnang.de@gmail.com>
2026-02-19 14:38:55 +0700

    merge onyl 50%

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -7,7 +7,7 @@
           <ul class="flex justify-center items-center gap-10">
             <li class="flex justify-center items-center text-xl text-black font-semibold">
               <NuxtLink 
-                to="/jobs" 
+                to="/" 
                 class="relative group transition duration-300">
                 <span class="group-hover:text-blue-500">Home</span>
                 <span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
@@ -15,7 +15,7 @@
             </li>
             <li class="flex justify-center items-center text-xl text-black font-semibold">
               <NuxtLink 
-                to="/jobs" 
+                to="/job-list" 
                 class="relative group transition duration-300">
                 <span class="group-hover:text-blue-500">Jobs</span>
                 <span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
@@ -24,7 +24,7 @@
             </li>
             <li class="flex justify-center items-center text-xl text-black font-semibold">
              <NuxtLink 
-                to="/jobs" 
+                to="/company-list" 
                 class="relative group transition duration-300">
                 <span class="group-hover:text-blue-500">Companies</span>
                 <span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
